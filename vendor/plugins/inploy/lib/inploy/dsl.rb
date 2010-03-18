e1698b1d29fad9c135adc2c7b9b0c4b62bca9cf9
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2010-03-18 09:43:05 -0400

    rake path updated

--- vendor/plugins/inploy/lib/inploy/dsl.rb
+++ vendor/plugins/inploy/lib/inploy/dsl.rb
@@ -18,7 +18,7 @@ module Inploy
     end
 
     def rake(command)
-      run "rake #{command}"
+      run "/opt/ruby-enterprise-1.8.6-20090610/bin/rake #{command}"
     end
 
     def rake_if_included(command)
